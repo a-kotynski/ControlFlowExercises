d7aa7c83996309e97c6f3469da37adefe154f886
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-29 15:32:45 +0200

    Add project files.

---
+++ ControlFlowExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-tutorial-for-beginners/learn/lecture/2910704#content
+namespace ControlFlowExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex. 1
+            //var i = 0;
+            //while(i <= 100)
+            //{
+            //    if (i%3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //Ex. 2
+        //    long sum = 0;
+        //    while (true)
+        //    {
+        //        Console.WriteLine("Type a number or \"ok\" to exit: \n");
+        //        var input = Console.ReadLine();
+                
+        //        if (input == "ok")
+        //        {
+        //            break;
+        //        }
+        //            var number = Convert.ToInt64(input);
+        //            sum += number;
+
+        //        Console.WriteLine($"Sum of the numbers is: {sum}");
+                
+        //        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //    }
+            //Ex. 3
+
+
+        }
+    }
+}
